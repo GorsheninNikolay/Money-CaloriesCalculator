0d0da1cb6c86e054d34d5d1cd3ccf99fd4139087
Nikolay <kolya.gorshenin.2002@gmail.com>
2021-03-18 18:51:21 +0300

    I am here

--- homework.py
+++ homework.py
@@ -1,76 +1,78 @@
-from typing import List, Any
 import datetime as dt
+
+DATE_FORMAT = "%d.%m.%Y"
 
 
 class Record:
-    def __init__(self, amount: float, comment: str, date: dt.datetime = None):
+    def __init__(self, amount: float, comment: str, date: str = None):
         self.amount = amount
         self.comment = comment
-        self.date = date
-        if self.date is None:
+        if date is None:
             self.date = dt.date.today()
         else:
-            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+            self.date = dt.datetime.strptime(date, DATE_FORMAT).date()
 
 
 class Calculator:
-    count = 0
-    week_count = 0
-
     def __init__(self, limit: float) -> None:
         self.limit = limit
-        self.records: List[Any] = []
+        self.records = []
 
-    def add_record(self, cls: Record) -> None:
-        self.records.append(cls)
-        self.days = dt.datetime.today().date() - cls.date
-        if self.days >= dt.timedelta(days=0):
-            if cls.date == dt.datetime.today().date():
-                self.count += cls.amount
-                self.week_count += cls.amount
-            elif self.days <= dt.timedelta(days=7):
-                self.week_count += cls.amount
+    def add_record(self, new_record: Record) -> None:
+        self.records.append(new_record)
 
     def get_today_stats(self) -> float:
+        TODAY = 0
         for record in self.records:
-            if record.date == dt.datetime.today():
-                self.count += record.amount
-        return self.count
+            if record.date == dt.date.today():
+                TODAY += record.amount
+        return TODAY
 
     def get_week_stats(self) -> float:
-        return self.week_count
+        WEEK_COUNT = 0
+        for record in self.records:
+            days = dt.date.today() - record.date
+            if days <= dt.timedelta(days=7) and days >= dt.timedelta(days=0):
+                WEEK_COUNT += record.amount
+        return WEEK_COUNT
 
 
 class CashCalculator(Calculator):
+    RUB_RATE = 1
     USD_RATE = 73.33
     EURO_RATE = 87.7
 
-    def get_today_cash_remained(self, type: str) -> Any:
-        currency = {'rub': 1,
+    def get_today_cash_remained(self, currency_code: str) -> str:
+        CASH_TODAY = super().get_today_stats()
+        currency = {'rub': self.RUB_RATE,
                     'usd': self.USD_RATE,
                     'eur': self.EURO_RATE}
         type_currency = {'rub': 'руб',
                          'usd': 'USD',
                          'eur': 'Euro'}
-        if self.count == self.limit:
+        if CASH_TODAY == self.limit:
             return 'Денег нет, держись'
-        elif self.count > self.limit:
-            rem = self.count - self.limit
-            return (f'Денег нет, держись: '
-                    f'твой долг - {round(rem / currency[type], 2)} '
-                    f'{type_currency[type]}')
+        elif CASH_TODAY > self.limit:
+            REMAIN = CASH_TODAY - self.limit
+            REMAIN = round(REMAIN / currency[currency_code], 2)
+            return ('Денег нет, держись: '
+                    f'твой долг - {REMAIN} '
+                    f'{type_currency[currency_code]}')
         else:
-            rem = self.limit - self.count
-            return (f'На сегодня осталось '
-                    f'{round(rem / currency[type], 2)} '
-                    f'{type_currency[type]}')
+            REMAIN = self.limit - CASH_TODAY
+            REMAIN = round(REMAIN / currency[currency_code], 2)
+            return ('На сегодня осталось '
+                    f'{REMAIN} '
+                    f'{type_currency[currency_code]}')
 
 
 class CaloriesCalculator(Calculator):
-    def get_calories_remained(self) -> Any:
-        if self.count <= self.limit:
-            return (f'Сегодня можно съесть что-нибудь ещё,'
-                    f' но с общей калорийностью не более '
-                    f'{self.limit - self.count} кКал')
-        else:
+    def get_calories_remained(self) -> str:
+        TODAY = super().get_today_stats()
+        if TODAY < self.limit:
+            REMAIN = self.limit - TODAY
+            return ('Сегодня можно съесть что-нибудь ещё,'
+                    ' но с общей калорийностью не более '
+                    f'{REMAIN} кКал')
+        if TODAY >= self.limit:
             return 'Хватит есть!'
